586bbb7f010af65398555e557168c68cf3767e4f
cpm-128 <cpm-128@users.noreply.github.com>
2022-04-26 13:36:17 -0400

    added checks and alerts of each robot's health

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,15 +13,29 @@ console.log(enemyName,enemyHealth,enemyAttack);
 
 var fight = function() {
     // alert players the round is starting
-    window.alert("Welcome to Robot Gladiators!");
+        window.alert("Welcome to Robot Gladiators!");
     // enemyHealth - playerAttack and update enemyHealth with result
-    enemyHealth = enemyHealth - playerAttack;
+        enemyHealth = enemyHealth - playerAttack;
     // log message to console to confirm math worked
-    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    // check enemyName health is <= 0
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
     // playerHealth - enemyAttack and update playerHealth with result
-    playerHealth = playerHealth - enemyAttack;
+        playerHealth = playerHealth - enemyAttack;
     // log message to confole to confirm math worked
-    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    // check playerName health is <=0
+        if (playerHealth <=0) {
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
 };
 
 fight();
