8fa982ab79e712e0d7fe75681d8eccd57d51404c
cpm-128 <cpm-128@users.noreply.github.com>
2022-04-27 12:30:53 -0400

    added startGame and endGame functions + commented out broken for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,47 @@ var enemyAttack = 12;
 
 console.log(enemyNames,enemyHealth,enemyAttack);
 
+// function to start a new game
+var startGame = function() {
+    // reset play status
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for (var i = 0; i < enemyNames.length; i++) {
+        if(playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle. GAME OVER.");
+            break;
+        }
+    }
+    //play again or end game?
+    endGame();
+};
+// end of function to start a new game
+
+// function to end entire game or play again
+var endGame = function() {
+    // if player is still alive, player wins, else, you lost
+    if (playerHealth > 0) {
+        window.alert("Great job, you survived the game. You now have a score of " + playerMoney + ".");
+    } else {
+        window.alert("You lost your robot in battle. ☠️")
+    }
+    // play again?
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        //restart the game
+        startGame();
+    } else
+        //end game entire game 
+        window.alert("Thank you for playing Robot Gladiators. Play again soon.");
+}
+// end of function to end entire game
+
 var fight = function(enemyName) {
     // repeat and execute as long as the enemy-robot is alive
     while(enemyHealth > 0 && playerHealth > 0) {
@@ -63,22 +104,25 @@ var fight = function(enemyName) {
     } //end of while loop
 } // end of fight function
 
-// the for loop is BELOW defining the fight function bc we need to already know how the fight function in this loop
-for(var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-        // identify the round
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
-        //pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames[i];
-        //reset enemyHealth before starting a new fight
-        enemyHealth = 50;
-        //pass the pickedEnemyName into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName)
-    }
-    else {
-        window.alert("You have lost your robot in battle. Game Over.");
-        break;
-    }
-}
+// I BELIEVE THE BELOW CODE IS UNNECESSARY FOR THE GAME TO FUNCTION PROPERLY
+// // the for loop is BELOW defining the fight function bc we need to already know how the fight function in this loop
+// for(var i = 0; i < enemyNames.length; i++) {
+//     if (playerHealth > 0) {
+//         // identify the round
+//         window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+//         //pick new enemy to fight based on the index of the enemyNames array
+//         var pickedEnemyName = enemyNames[i];
+//         //reset enemyHealth before starting a new fight
+//         enemyHealth = 50;
+//         //pass the pickedEnemyName into the fight function, where it will assume the value of the enemyName parameter
+//         fight(pickedEnemyName)
+//     }
+//     else {
+//         window.alert("You have lost your robot in battle. Game Over.");
+//         break;
+//     }
+// }
+// // end for loop
 
-//fight();
+// call the startGame function to be executed when the page loades
+startGame();
